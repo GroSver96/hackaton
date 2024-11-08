d4ac282d4b44564e939bfa7df5ea04bc190f9013
Grover Hernani <hernanigrover@gmail.com>
2024-11-08 01:30:42 -0400

    css inicio hospital

--- src/app/component/inicio-hospital/inicio-hospital.component.ts
+++ src/app/component/inicio-hospital/inicio-hospital.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../service/auth.service';
 import { Router } from '@angular/router';
 import { getAuth, signOut } from 'firebase/auth';
+
 @Component({
   selector: 'app-inicio-hospital',
   templateUrl: './inicio-hospital.component.html',
@@ -9,17 +10,10 @@ import { getAuth, signOut } from 'firebase/auth';
 })
 export class InicioHospitalComponent {
 
-  constructor(private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  onLogout() {
-    const auth = getAuth();  // Obtén la instancia de autenticación
-    signOut(auth)
-      .then(() => {
-        console.log('Sesión cerrada');
-        this.router.navigate(['/login']); // Redirigir al login después de cerrar sesión
-      })
-      .catch((error) => {
-        console.error('Error al cerrar sesión:', error);
-      });
+  onLogout(){
+    this.authService.logout()
+    this.router.navigate(['/login']); 
   }
 }
